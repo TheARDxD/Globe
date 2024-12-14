63329327eb372c6be2116b59b2803c7f87f9b29a
TheARD <69312435+TheARDxD@users.noreply.github.com>
2024-12-14 17:59:31 +0530

    Updated Vite Dependencies

--- globe/vite.config.js
+++ globe/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['three']  // Add 'three' to the external array
+  resolve: {
+    alias: {
+      // Ensure 'three/examples' is correctly resolved
+      '@three': path.resolve(__dirname, 'node_modules/three')
     }
   }
 });
